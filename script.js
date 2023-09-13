5d3efcfc40145fffec1b61d37db616404dff8363
Estevo Vinicius <viniestevo33@gmail.com>
2023-09-13 16:01:05 -0300

    recycling code, functions

--- script.js
+++ script.js
@@ -5,16 +5,33 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco')
 });
+
+// focoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'foco')
+//     banner.setAttribute('src', '/imagens/foco.png')
+// });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 });
 
+// curtoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-curto')
+//     banner.setAttribute('src', '/imagens/descanso-curto.png')
+// });
+
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 });
+
+// longoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-longo')
+//     banner.setAttribute('src', '/imagens/descanso-longo.png')
+// });
+
+function alterarContexto (contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
