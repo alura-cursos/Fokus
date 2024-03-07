8ab1603e1bd4abf36962d5f56afc6c04a17d7720
Melo <devmelo.tech@gmail.com>
2024-03-07 18:20:57 -0300

    Renomeando algumas imagens e classes, para um código melhor. Evitando assim, código muito grande.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const html = document.querySelector('html');
+const banner = document.querySelector('.app__image');
 
 const btn = document.querySelectorAll('.app__card-button');
 
 
 //Busca dentro da lista na const btn todos os botões e informa o conteúdo do clique.
 btn.forEach((elemento) =>{
-    elemento.addEventListener("click", (evento) =>{
-        console.log(evento.target.textContent);
-
-        if(evento.target.textContent === "Foco"){
-            html.setAttribute('data-contexto', 'foco');
-        } else if(evento.target.textContent === "Descanso curto"){
-            html.setAttribute('data-contexto', 'descanso-curto');
-        } else{
-            html.setAttribute('data-contexto', 'descanso-longo');
-        }
+    elemento.addEventListener("click", () =>{
+        const context = elemento.getAttribute('data-contexto');
+        //console.log(moodValue);
+        changeContext(context);    
     })
 })
+
+function changeContext(context){
+        html.setAttribute('data-contexto', context);
+        banner.setAttribute('src', "/imagens/"+context+".png");
+}
