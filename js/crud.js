9b14b60f2231af25f56c5dbd1870c70b1626857d
Melo <devmelo.tech@gmail.com>
2024-03-07 23:48:38 -0300

    localStorage

--- js/crud.js
+++ js/crud.js
@@ -2,8 +2,23 @@
 
 const addBtn = document.querySelector('.app__button--add-task');
 const activityForm = document.querySelector('.app__form-add-task');
+const formTxtArea = document.querySelector('.app__form-textarea');
 
-addBtn.addEventListener('click', () =>{
+const tasks = [];
+
+addBtn.addEventListener('click', () => {
     //Toggle = alterna a classe. Ou seja, clicando no botão ira esconder e exibir o form.
     activityForm.classList.toggle('hidden');
+})
+
+activityForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const task = {
+        description: formTxtArea.value
+    }
+    tasks.push(task);
+
+    // Usar biblioteca JSON para poder guardar informações na localStorage
+    localStorage.setItem('tarefas', JSON.stringify(tasks));
 })
